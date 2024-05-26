ea880f0c42935fe0957c83316f78bd5dc26fd3b5
SHUU <59950875+jesniey11@users.noreply.github.com>
2024-05-26 23:56:04 +0900

    [Add] New Component

---
+++ Source/study04/TriggerComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/BoxComponent.h"
+#include "TriggerComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STUDY04_API UTriggerComponent : public UBoxComponent
+{
+	GENERATED_BODY()
+	
+};
