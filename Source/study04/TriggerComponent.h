bdee83e8a0b57fd03385cf3c36dcd583515f33f3
SHUU <59950875+jesniey11@users.noreply.github.com>
2024-05-27 00:28:23 +0900

    [Chore] Test TriggerComponent connection

--- Source/study04/TriggerComponent.h
+++ Source/study04/TriggerComponent.h
@@ -9,9 +9,20 @@
 /**
  * 
  */
-UCLASS()
+UCLASS( ClassGroup = (Custom), meta = (BlueprintSpawnableComponent) )
 class STUDY04_API UTriggerComponent : public UBoxComponent
 {
 	GENERATED_BODY()
-	
+
+public:
+	// Sets default values for this component's properties
+	UTriggerComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 };
